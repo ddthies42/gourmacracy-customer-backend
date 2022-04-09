78d8c99de9096e8a54cf38e2cfbcf06a82352952
ddthies42 <trant29@spu.edu>
2022-04-08 21:31:23 -0700

    added backend code from Home Library project

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routers/index');
+var bookRouter = require('./routers/books');
+var db = require('./db');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// To be able to use the API everywhere
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  //response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.header('Access-Control-Allow-Methods', 'GET,POST,PUT, PATCH, DELETE');
+  //response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/api/books', bookRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
