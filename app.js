75b76f84c831bd6117bcfd32c4fae2935ddeecdc
StylesP23 <riley.pleas@gmail.com>
2022-04-09 17:16:59 -0700

    made changes to app.js, users.js in routes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((request, response, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/api/books', bookRouter);
+app.use('/api/users', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://ddthies:Gourmacracy123@netcentriccluster.26wkw.mongodb.net/BookDatabase?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://ddthies:Gourmacracy123@netcentriccluster.26wkw.mongodb.net/Gourmacracy?retryWrites=true&w=majority';
 
 mongoose
     .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
