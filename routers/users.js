ac4797267aebad67594f7518c9edd842f9931eae
ddthies42 <trant29@spu.edu>
2022-04-12 14:17:41 -0700

    change from Riley's branch

--- routers/users.js
+++ routers/users.js
@@ -7,7 +7,7 @@ function HandleError(response, reason, message, code){
     response.status(code || 500).json({"error": message});
 }
 
-//Gets all the users
+//Gets all the users --
 router.get('/', (request, response, next)=>{
     let word = request.query['name'];
     if (word){
