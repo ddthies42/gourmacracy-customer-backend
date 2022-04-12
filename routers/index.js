a60028811c89bb76f648a1451fea9146786f8384
ddthies42 <trant29@spu.edu>
2022-04-11 21:24:24 -0700

    Update index.js
    Gourmacracy APIs

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Book APIs' });
+  res.render('index', { title: 'Gourmacracy APIs' });
 });
 
 module.exports = router;
