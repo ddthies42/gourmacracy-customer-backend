c0165433d1c639eaeedd7302e70e633120eaf20f
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2022-05-10 08:36:24 -0700

    Added get for menu items

--- routers/rating.js
+++ routers/rating.js
@@ -8,7 +8,31 @@ function HandleError(response, reason, message, code){
     response.status(code || 500).json({"error": message});
 }
 
-
+//Gets all the menu items ---
+router.get('/', (request, response, next)=>{
+    let name = request.query['menuItem'];
+    if (name){
+        MenuSchema
+            .find({"menuItem": name})
+            .exec( (error, name) =>{
+                if (error){
+                    response.send({"error": error});
+                }else{
+                    response.send(menu);
+                }
+            });
+    }else{
+        MenuSchema
+            .find()
+            .exec( (error, menu) =>{
+                if (error){
+                    response.send({"error": error});
+                }else{
+                    response.send(menu);
+                }
+            });
+    }
+});
 
 
 
