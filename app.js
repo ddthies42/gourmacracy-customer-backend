ae486f1d49bd9dbc81bc7247ffa6ed1a95cb9283
StylesP23 <riley.pleas@gmail.com>
2022-05-15 22:03:40 -0700

    more session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var createError = require('http-errors');
 var express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,7 +24,11 @@ app.set('view engine', 'pug');
 
 app.use(cors());
 
-app.use(session({secret: 'magical'}));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
