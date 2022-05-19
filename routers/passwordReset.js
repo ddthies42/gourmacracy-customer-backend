e737a691e1114610b3c86e5534dec6b3c3f5ae3d
StylesP23 <riley.pleas@gmail.com>
2022-05-19 12:50:45 -0700

    changes to resetPassword

--- routers/passwordReset.js
+++ routers/passwordReset.js
@@ -8,24 +8,25 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
     try {
-        const schema = Joi.object({ email: Joi.string().email().required() });
-        const { error } = schema.validate(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
+     //   const schema = Joi.object({ email: Joi.string().email().required() });
+       // const { error } = schema.validate(req.body);
+       // if (error) return res.status(400).send(error.details[0].message);
+       UserSchema
 
-        const user = await User.findOne({ email: req.body.email });
-        if (!user)
+        .findOne({ email: req.body.email });
+        if (!User)
             return res.status(400).send("user with given email doesn't exist");
 
-        let token = await Token.findOne({ userId: user._id });
+        let token = await Token.findOne({ userId: User._id });
         if (!token) {
             token = await new Token({
-                userId: user._id,
+                userId: User._id,
                 token: crypto.randomBytes(32).toString("hex"),
             }).save();
         }
 
-        const link = `${process.env.BASE_URL}/password-reset/${user._id}/${token.token}`;
-        await sendEmail(user.email, "Password reset", link);
+        const link = `${process.env.BASE_URL}/password-reset/${User._id}/${token.token}`;
+        await sendEmail(User.email, "Password reset", link);
         res.send("password reset link sent to your email account");
     } catch (error) {
         res.send("An error occured");
@@ -35,21 +36,21 @@ router.post("/", async (req, res) => {
 
 router.post("/:userId/:token", async (req, res) => {
     try {
-        const schema = Joi.object({ password: Joi.string().required() });
-        const { error } = schema.validate(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-
-        const user = await User.findById(req.params.userId);
-        if (!user) return res.status(400).send("invalid link or expired");
+       // const schema = Joi.object({ password: Joi.string().required() });
+       // const { error } = schema.validate(req.body);
+       // if (error) return res.status(400).send(error.details[0].message);
+        UserSchema
+        .findById(req.params.userId);
+        if (!User) return res.status(400).send("invalid link or expired");
 
         const token = await Token.findOne({
-            userId: user._id,
+            userId: User._id,
             token: req.params.token,
         });
         if (!token) return res.status(400).send("Invalid link or expired");
 
-        user.password = req.body.password;
-        await user.save();
+        User.password = req.body.password;
+        await User.save();
         await token.delete();
         res.send("password reset sucessfully.");
     } catch (error) {
