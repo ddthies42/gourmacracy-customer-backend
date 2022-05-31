20569906a998ed5b18ee9843598fd86204e8528a
Peter-A-Felton <feltonp@spu.edu>
2022-05-31 00:07:04 -0700

    Removed backend sessions. Fixed nasty login bug that crashed backend. Removed old commented out code.

--- routers/users.js
+++ routers/users.js
@@ -7,13 +7,6 @@ function HandleError(response, reason, message, code){
     console.log('ERROR: ' + reason);
     response.status(code || 500).json({"error": message});
 }
-var sess;
-
-function get () {
-    // (A) GET FROM SESSION
-    //var sID = sessionStorage.getItem("sess._id");
-}
-
 
 //Gets all the users ---
 router.get('/', (request, response, next)=>{
@@ -55,38 +48,8 @@ router.get('/:id', (request, response, next) =>{
         });
 });
 
-// router.get('/:email', (request, response, next) =>{
-//     User
-//         .findById({"email": request.params.email}, (error, result) => {
-//             if (error){
-//                 response.status(500).send(error);
-//             }else if (result){
-//                 response.send(result._id);
-//             }else{
-//                 response.status(404).send({"email": request.params.email, "error": "Not Found"});
-//             }
-//         });
-// });
-
-
-// router.get('/itemId/:id', (request, response, next) =>{
-//     MenuSchema
-//         .findById({"_id": request.params.id}, (error, result) => {
-//             if (error){
-//                 response.status(500).send(error);
-//             }else if (result){
-//                 response.send(request.params.id);
-//             }else{
-//                 response.status(404).send({"id": request.params.id, "error": "Not Found"});
-//             }
-//         });
-// });
-
-
 //Register a User
 router.post('/', (req, response, next) => {
-    console.log("Hello!");
-    console.log(req);
     bcrypt.hash(req.body.password, 10).then((hash) => {
         const user = new User({
             name: req.body.name,
@@ -94,11 +57,10 @@ router.post('/', (req, response, next) => {
             password: hash
         });
         user.save( (error) => {
-            if (error){
+            if(error){
                 response.send({"error": error});
-            }else{
+            } else {
                 response.send(user.name);
-
             }
         });
     });
@@ -107,59 +69,39 @@ router.post('/', (req, response, next) => {
 
 //login page: storing and comparing email and password
 router.post('/signin', function (req, response) {
-    sess = req.session;
-    sess.logingin = "true";
-    console.log(req.body.email)
+
     User
     .find({"email": req.body.email})
     .exec( (error, userData) =>{
-        if (error){
-            response.send({"error": error});
-        }else{
-            if (!userData) {
-                           response.send('Email not found!');
+        if (error) {
+
+            // response.send({"error": error}); // Want to give message frontend can use.
+            response.send({'text': 'Error signing in. Try again later.', 'id': 'error', 'error': error});
+
+            // If the results are not blank, it is a user.
+        } else if (userData.length > 0) {
+
+              user = userData[0];
+
+              bcrypt.compare(req.body.password, user.password, function (err, result) {
+                   if (result == true) {
+
+                        if (user._id == "6281d69c6009f90004b69931") {
+                           response.send({'text': 'Admin Login Successful!', 'id': user._id});
                         } else {
-                            user = userData[0];
+                           response.send({'text': 'Login Successful!', 'id': user._id});
+                        }
 
-                            console.log(user);
-
-                bcrypt.compare(req.body.password, user.password, function (err, result) {
-                       if (result == true) {
-                            sess._id = user._id;
-                            //sessionStorage.setItem("sess._id", sess._id);
-
-                            console.log(user._id);
-                            if (user._id == "6281d69c6009f90004b69931") {
-                               response.send({'text': 'Admin Login Successful!', 'id': user._id});
-                            } else {
-                               response.send({'text': 'Login Successful!', 'id': user._id});
-                            }
-
-                       } else {
-                        response.send({'text':'Unable to login with this email address and password. ' +
-                            'Check your login information and try again.', 'id': 'error'});
-                       }
-                     });
-                   // });
+                   } else {
+                    response.send({'text':'Unable to login with this email address and password. ' +
+                        'Check your login information and try again.', 'id': 'error'});
                    }
-            console.log(user);
-
+             });
+        } else {
+          response.send({'text':'Unable to login with this email address and password. ' +
+              'Check your login information.', 'id': 'error'});
         }
     });
-
-//         if (!user) {
-//            response.send('Email not found!');
-//         } else {
-// bcrypt.compare(sess.password, user.password, function (err, result) {
-//        if (result == true) {
-//         sess.email = req.body.email;
-//         sess.password = req.body.password;
-//            response.send('Login Successful!');
-//        } else {
-//         response.send('Incorrect password');
-//        }
-//      });
- //   }
  });
 
 
@@ -236,62 +178,9 @@ router.delete('/:id', (request, response, next) => {
 
 //logout
 router.post('/logout/',(req,response) => {
-    //console.log(req.body.id);
-    req.session.destroy((err) => {
-        if(err) {
-            response.send("Backend token not deleted: " + err);
-            return console.log(err);
-        }
-        else {
-          response.send("Logged out successfully.");
-        }
-        
-    });
-
+  response.send("Logged out successfully.");
 });
-
-// This method should be a patch
-// Add order number + ,
-
-// method to append a user's orders array in database
-// router.patch('/purchase/:id', (request, response) => {
-//     const { id } = request.params
-//     const changes = request.body
-//     User
-//     .findOneAndUpdate(request.params.id, (error, result) => {
-//         if (error) {
-//             response.status(500).send(error);
-//         }else if (result){
-//             if (changes._id){
-//                 for (let field in changes){
-//                     result[field] += changes[field] + " ";
-//                 }
-//             }
-//             result.save((error, user)=>{
-//                 if (error){
-//                     response.status(500).send(error);
-//                 }
-//                 response.send(user);
-//             });
-//         }else{
-//             response.status(404).send({"id": request.params.id, "error":  "Not Found"});
-//         }
-//     });
-// });
 
 
 
 module.exports = router;
-
-
-// Friend.findOneAndUpdate(
-//     { _id: req.body.id },
-//     { $push: { friends: objFriends  } },
-//    function (error, success) {
-//          if (error) {
-//              console.log(error);
-//          } else {
-//              console.log(success);
-//          }
-//      });
-//  )
